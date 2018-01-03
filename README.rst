12f3943332579b99ff2bb4a188409550e2d0433a
Will Ballard <wballard@mailframe.net>
2018-01-03 08:18:51 -0600

    ancient quotes file for initial testing

---
+++ README.rst
@@ -0,0 +1,8 @@
+machine-learning-fortune
+========================
+
+You always wanted to learn how to make a `fortune` with machine learning, now
+is your time!
+
+::
+    machine-learning-fortune
--- setup.py
+++ setup.py
@@ -2,9 +2,12 @@
 Just the setup.
 '''
 
-from sys import platform
 
 from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 # Package details
 setup(
@@ -14,13 +17,16 @@ setup(
     author_email='wballard@mailframe.net',
     url='https://github.com/wballard/machine-learning-fortune',
     description='Make a `fortune` with machine learning!',
+    long_description=readme(),
     license='BSD 3-Clause License',
     packages=find_packages(),
+    scripts=['bin/machine-learning-fortune'],
     install_requires=[
         'keras>=2.1.2',
         'tensorflow>=1.4.1',
         'numpy>=1.13.1',
         'tqdm',
+        'docopt',
         'scikit-learn>=0.18.1'
     ],
     classifiers=[
